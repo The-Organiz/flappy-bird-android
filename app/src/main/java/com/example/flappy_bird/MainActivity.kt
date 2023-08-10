983b37c4bcf77bb4de9588fe3d4032c73d5c901e
govindnair <govindunair@gmail.com>
2023-08-09 23:49:16 -0400

    refactor

--- app/src/main/java/com/example/flappy_bird/MainActivity.kt
+++ app/src/main/java/com/example/flappy_bird/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.flappy_bird
 
-//import android.R
-//import android.R
-
 import android.animation.ValueAnimator
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.View
-import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.view.animation.LinearInterpolator
 import android.widget.ImageView
@@ -18,10 +14,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // set background
+        // set moving background
         setContentView(R.layout.activity_main)
+        moveBackground()
+        // animate static bird
+        val bird = bounceBird(flapBird())
+    }
 
-        // move background
+    fun moveBackground(){
         val backgroundOne = findViewById<ImageView>(R.id.background1);
         val backgroundTwo = findViewById<ImageView>(R.id.background2);
         val animator = ValueAnimator.ofFloat(0.0f, -1.0f) // change sign for reverse
@@ -36,18 +36,22 @@ class MainActivity : AppCompatActivity() {
             backgroundTwo.translationX = translationX + width // change to - for reverse
         }
         animator.start()
+    }
 
-        // animate flapping bird
+    fun flapBird(): ImageView {
         val img = findViewById<View>(R.id.bird_fly) as ImageView
         img.setBackgroundResource(R.drawable.bird_fly_animation)
         val frameAnimation = img.background as AnimationDrawable
         frameAnimation.start()
+        return img
+    }
 
-        // animate bouncing bird
+    fun bounceBird(img: ImageView): ImageView{
         val animUpDown = AnimationUtils.loadAnimation(
             applicationContext,
             R.anim.up_down
         )
         img.startAnimation(animUpDown)
+        return img
     }
 }
