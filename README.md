d7577a7b049729fabccabe0b9e8af26658edd641
govindnair <govindunair@gmail.com>
2023-08-11 16:46:18 -0400

    add gravity

---
+++ README.md
@@ -0,0 +1,23 @@
+kotlin project
+
+required local directory structure:
+    flappybird:
+        app:
+            manifests
+            java
+                *
+                com.example.flappy_bird
+                    MainActivity
+            res
+                *
+                anim
+                    updown.xml
+                drawable
+                    *
+                    background.jpeg
+                    bird*.png
+                    bird_fly_animation.xml
+                    ground.jpeg
+                layout
+                    activity_main.xml
+        Gradle Scripts
--- app/src/main/java/com/example/flappy_bird/MainActivity.kt
+++ app/src/main/java/com/example/flappy_bird/MainActivity.kt
@@ -11,20 +11,24 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bird: ImageView;
+    private var started: Boolean = false;
+    private var posY: Float = 0f;
+    private var dy: Float = 0.05f;
+    private val ddy: Float = 0.01f;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // set moving background
         setContentView(R.layout.activity_main)
         moveGround()
         // animate static bird
-        val bird = bounceBird(flapBird())
+        bird = bounceBird(flapBird())
     }
 
-    fun moveGround(){
+    private fun moveGround(){
         val ground1 = findViewById<ImageView>(R.id.ground1);
         val ground2 = findViewById<ImageView>(R.id.ground2);
-        val animator = ValueAnimator.ofFloat(0.0f, -1.0f) // change sign for reverse
+        val animator = ValueAnimator.ofFloat(0.0f, -1.0f)
         animator.repeatCount = ValueAnimator.INFINITE
         animator.interpolator = LinearInterpolator()
         animator.duration = 2500L
@@ -33,12 +37,12 @@ class MainActivity : AppCompatActivity() {
             val width = ground1.width.toFloat()
             val translationX = width * progress
             ground1.translationX = translationX
-            ground2.translationX = translationX + width // change to - for reverse
+            ground2.translationX = translationX + width
         }
         animator.start()
     }
 
-    fun flapBird(): ImageView {
+    private fun flapBird(): ImageView {
         val img = findViewById<View>(R.id.bird_fly) as ImageView
         img.setBackgroundResource(R.drawable.bird_fly_animation)
         val frameAnimation = img.background as AnimationDrawable
@@ -46,12 +50,58 @@ class MainActivity : AppCompatActivity() {
         return img
     }
 
-    fun bounceBird(img: ImageView): ImageView{
+    private fun bounceBird(img: ImageView): ImageView{
         val animUpDown = AnimationUtils.loadAnimation(
             applicationContext,
             R.anim.up_down
         )
         img.startAnimation(animUpDown)
         return img
+    }
+
+    fun screenTapped(view: View?) {
+        bird = findViewById<ImageView>(R.id.bird_fly);
+
+        val animator = ValueAnimator.ofFloat(1f, 0.0f)
+        animator.interpolator = LinearInterpolator()
+        animator.duration = 1000L
+        animator.addUpdateListener { animation ->
+            val progress = animation.animatedValue as Float
+            val height = bird.height.toFloat()
+            val translationY = height * progress
+            bird.translationY = translationY
+        }
+        animator.start()
+
+        if(!started) {
+            started = true
+            gameLoop()
+        }
+    }
+
+    private fun gameLoop(){
+        var animator = ValueAnimator.ofFloat(0f, 1f)
+        animator.repeatCount = ValueAnimator.INFINITE
+        animator.interpolator = LinearInterpolator()
+        var end = posY + dy
+        animator.addUpdateListener { _ ->
+            dropBird(posY, end)
+            dy += ddy
+            posY = end
+            end += dy
+        }
+        animator.start()
+    }
+
+    private fun dropBird(start: Float, end: Float){
+        var animator = ValueAnimator.ofFloat(start, end)
+        animator.interpolator = LinearInterpolator()
+        animator.addUpdateListener { animation ->
+            val progress = animation.animatedValue as Float
+            val height = bird.height.toFloat()
+            val translationY = height * progress
+            bird.translationY = translationY
+        }
+        animator.start()
     }
 }
